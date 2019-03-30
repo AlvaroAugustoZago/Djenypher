79a1deb3531ba15ab5c09b814ec67a0807a9159b
Bruno <brubatera5@gmail.com>
2019-03-30 17:22:39 -0300

    Tela chat usuário

--- app/src/main/java/com/example/alfarrthebard/chatfaq/Main.java
+++ app/src/main/java/com/example/alfarrthebard/chatfaq/Main.java
@@ -1,7 +1,13 @@
 package com.example.alfarrthebard.chatfaq;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +15,24 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.sendMessage);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                TextInputLayout edit = (TextInputLayout) findViewById(R.id.caixaTexto);
+                TextView tview = (TextView) findViewById(R.id.textChat);
+                String result = edit.getEditText().getText().toString();
+                tview.append("Você: " + result + "\n");
+                tview.setMovementMethod(new ScrollingMovementMethod());
+                edit.getEditText().setText("");
+
+            }
+        });
     }
+
+
 }
+
+
